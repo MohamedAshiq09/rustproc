dd43dbb9a2a2a6aba34d76dcb64cc3a9e65ed20a
MohamedAshiq09 <mohamedashiq782gmail.com>
2024-11-04 13:58:51 +0000

    update

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+fn main() {
+    // boolean operators 
+    //print!("1<4 is {}", 1<4);
+    //print! ("1==5 is {}", 1==5);
+    // lazy boolean and && operator 
+    //print!("1==1 && 1>3 is {}", 1==1 && 1>3);
+    // lazy || or operator 
+    //print!("1==1 || 1>3 is {}", 1==1 || 1>3)
+    // condition statement 
+    //let cond = 2 < 4;
+     //print! ("{}", cond);
+    // comparing integer with float 
+    //let cond2 = (2 as f32) < 4.4;
+    //print!("{}",cond2);
+    //let cond = 2 <4 ;
+    //let cond2 = false && cond;
+    //print!("{}", cond2);
+    // rust not operator 
+    //let cond = 2 < 4;
+    //let cond2 = !(true && cond);
+    //print!("{}",cond2);
+    // rust conditional operator
+    let food = "rice";
+
+    if food == "rice" {
+        print!("it's nice");
+    } else if food == "bread" {
+        print! ("it's awesome");    
+    } else {
+        print!("not bad");
+    }
+
+
+
+}
